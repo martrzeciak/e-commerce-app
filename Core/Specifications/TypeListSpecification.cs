065cc0843ebf35eedd24c8c5c3047556987f88d3
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-08-09 18:17:02 +0200

    Add generic repository, implement specification pattern

---
+++ Core/Specifications/TypeListSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Core.Specifications
+{
+    public class TypeListSpecification : BaseSpecification<Product, string>
+    {
+        public TypeListSpecification()
+        {
+            AddSelect(x => x.Type);
+            ApplyDistinct();
+        }
+    }
+}
